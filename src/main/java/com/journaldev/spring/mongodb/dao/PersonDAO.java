c82121b73241035e213ea8a5d2724fdd3d03a3e6
Gunavardhan <gunavardhan@newtel.in>
2020-09-18 17:24:42 +0530

    Moving project

---
+++ src/main/java/com/journaldev/spring/mongodb/dao/PersonDAO.java
@@ -0,0 +1,18 @@
+package com.journaldev.spring.mongodb.dao;
+
+import java.util.List;
+
+import com.journaldev.spring.mongodb.model.Person;
+
+public interface PersonDAO {
+
+	 void create(Person p);
+	
+	 Person readById(String id);
+	 
+	 List<Person> getAllPersons();
+	
+	 void update(Person p);
+	
+	 int deleteById(String id);
+}
