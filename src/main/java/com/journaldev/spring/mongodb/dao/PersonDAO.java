1ec81e06ecd4306f07285c4f55d737d0779c85b9
Gunavardhan <gunavardhan@newtel.in>
2020-09-18 18:33:39 +0530

    gunavardhan : test commit branch new

--- src/main/java/com/journaldev/spring/mongodb/dao/PersonDAO.java
+++ src/main/java/com/journaldev/spring/mongodb/dao/PersonDAO.java
@@ -6,12 +6,8 @@ import com.journaldev.spring.mongodb.model.Person;
 
 public interface PersonDAO {
 	 void create(Person p);
-	 
 	 Person readById(String id);
-	 
 	 List<Person> getAllPersons();
-	 
 	 void update(Person p);
-	 
 	 int deleteById(String id);
 }
